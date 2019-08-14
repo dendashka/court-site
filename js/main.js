860b6ad09efaa7edf8992a5293452985e11a7397
Denis Dashkevich <d-dashkevich@mail.ru>
2019-08-14 11:19:07 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>'
+    });
+});
